d5124199ae6b35d95f5833ea15f2cb2ce4edfd93
72088838 <272717803@qq.com>
2020-09-12 18:33:40 +0800

    list map转换

--- demo-test/src/main/java/org/demo/jdk8/StremTest.java
+++ demo-test/src/main/java/org/demo/jdk8/StremTest.java
@@ -15,8 +15,23 @@ import java.util.stream.Stream;
  */
 public class StremTest {
     public static void main(String[] args) {
-        sequential();
-        parallel();
+//        sequential();
+//        parallel();
+        test();
+    }
+
+    public static void test(){
+        //parallelStream 得到的并行流 并行流内部使用了默认的ForkJoinPool（7.2节会进一步讲到分支/合并框架），
+        // 它默认的线程数量就是你的处理器数量，这个值是由Runtime.getRuntime().available- Processors()得到的。 但是你可以通过系统属性java.util.concurrent.ForkJoinPool.common. parallelism来改变线程池大小，如下所示： System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism","12"); 这是一个全局设置，因此它将影响代码中所有的并行流。反过来说，目前还无法专为某个 并行流指定这个值。一般而言，让ForkJoinPool的大小等于处理器数量是个不错的默认值
+        List<String> list = new ArrayList<String>();
+        for(int i=0;i<100;i++){
+            list.add(""+i);
+        }
+        list = list.parallelStream().filter(s->{return s.contains("1");}).collect(Collectors.toList());
+        //字符串转map
+        Map<String,String> map = list.stream().collect(Collectors.toMap(String::toString, a -> a));
+        //list 转list
+        List<Integer> lis = list.parallelStream().filter(s->{return s.contains("1");}).map(Integer::new).collect(Collectors.toList());
     }
     public static void parallel(){
         List<String> list = new ArrayList<String>();
